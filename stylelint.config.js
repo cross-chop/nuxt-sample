40b96fa6f3215a99215c66066431f301410e50d9
Yuzo Sakai <sakai@cross-chop.jp>
2020-07-10 21:00:51 +0900

    stylelintを自動で走らせる

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,5 +1,10 @@
 module.exports = {
-  extends: ['stylelint-config-standard', 'stylelint-config-prettier'],
+  extends: [
+    'stylelint-config-standard',
+    'stylelint-config-recess-order',
+    'stylelint-config-recommended-scss',
+    'stylelint-prettier/recommended',
+  ],
   // add your custom config here
   // https://stylelint.io/user-guide/configuration
   rules: {},
